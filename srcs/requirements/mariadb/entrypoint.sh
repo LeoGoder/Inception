b690abedf2373f8359e3f49ee3ae51c16a5f9858
lgoderne <lgoderne@Inception.myguest.virtualbox.org>
2026-08-21 10:47:07 +0200

    [FIX] race condition with mariadb with sleep now proper loop added

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -8,20 +8,24 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 fi
 
 mysqld_safe &
-sleep 5
+while [ ! -S /run/mysqld/mysqld.sock ]; do
+    sleep 1
+done
 
 source /run/secrets/db
 source /run/secrets/user
 
-mariadb -e "
-CREATE DATABASE IF NOT EXISTS ${DBNAME};
-CREATE USER IF NOT EXISTS '${DBUSER}'@'%' IDENTIFIED BY '${UMDP}';
-GRANT ALL PRIVILEGES ON ${DBNAME}.* TO '${DBUSER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DBMDP}';
-FLUSH PRIVILEGES;
-USE ${DBNAME};
-CREATE TABLE IF NOT EXISTS users (id INT, nom varchar(50));
-SHOW TABLES;
-"
+if [ ! -d "/var/lib/mysql/${DBNAME}" ]; then
+    mariadb -e "
+    CREATE DATABASE IF NOT EXISTS ${DBNAME};
+    CREATE USER IF NOT EXISTS '${DBUSER}'@'%' IDENTIFIED BY '${UMDP}';
+    GRANT ALL PRIVILEGES ON ${DBNAME}.* TO '${DBUSER}'@'%';
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${DBMDP}';
+    FLUSH PRIVILEGES;
+    USE ${DBNAME};
+    CREATE TABLE IF NOT EXISTS users (id INT, nom varchar(50));
+    SHOW TABLES;
+    "
+fi
 
 wait
